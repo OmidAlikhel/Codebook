3347064cde5b7acff9ca4ce72951990a140c87c8
Omid Alikhtl <omidrazi_1989@yahoo.com>
2024-02-13 20:42:10 -0800

    adding changes of login as guest

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -12,16 +12,39 @@ export const Login = () => {
 
   async function handleLogin(event) {
     event.preventDefault();
-    try {const authDetail = {
-      email: email.current.value,
-      password: password.current.value,
-    };
-    const data = await login(authDetail);
-    data.accessToken ? navigate("/products") : toast.error(data); }
-    catch (error) {
-      toast.error(error.message, {closeButton: true, position : "bottom-center"})
+    try {
+      const authDetail = {
+        email: email.current.value,
+        password: password.current.value,
+      };
+      const data = await login(authDetail);
+      data.accessToken ? navigate("/products") : toast.error(data);
+    } catch (error) {
+      toast.error(error.message, {
+        closeButton: true,
+        position: "bottom-center",
+      });
     }
-    
+  }
+
+  async function handleGuestLoing(event) {
+    event.preventDefault();
+    email.current.value = "omid@example.com";
+    password.current.value = "omid123";
+    try {
+      const authDetail = {
+        email: email.current.value,
+        password: password.current.value,
+      };
+
+      const data = await login(authDetail);
+      data.accessToken ? navigate("/products") : toast.error(data);
+    } catch (error) {
+      toast.error(error.message, {
+        closeButton: true,
+        position: "bottom-center",
+      });
+    }
   }
   return (
     <main>
@@ -70,7 +93,12 @@ export const Login = () => {
           Log In
         </button>
       </form>
-      {/* <button className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> */}
+      <button
+        onClick={handleGuestLoing}
+        className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Login As Guest
+      </button>
     </main>
   );
 };
